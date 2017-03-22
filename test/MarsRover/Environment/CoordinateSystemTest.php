321ff2a46a3c410cab3f47a5928b4d38bd71e7fc
laola1technik <laola1technik@users.noreply.github.com>
2017-03-22 17:40:18 +0100

    add TODOs

--- test/MarsRover/Environment/CoordinateSystemTest.php
+++ test/MarsRover/Environment/CoordinateSystemTest.php
@@ -4,6 +4,17 @@ namespace MarsRover\Environment;
 class CoordinateSystemTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * TODO:
+     * 1. rename coordinate to navigation
+     * 2. test for navigation
+     *  + forward 4 Directions
+     *  + backward 4 Directions
+     * 3. Wrapping
+     * Open Question:
+     * Was ist mit Karte oder Welt, wo kommen Hindernisse her, wer bestimmt die Größe
+     */
+
+    /**
      * @test
      */
     public function shouldInitiallyBeZeroZero()
@@ -14,5 +25,4 @@ class CoordinateSystemTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(0, $position->getX());
         $this->assertSame(0, $position->getY());
     }
-
 }
