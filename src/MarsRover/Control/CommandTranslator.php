73d622c056eceb0bbfc2e3f82e583cdc74ec5168
laola1technik <laola1technik@users.noreply.github.com>
2017-08-31 16:10:30 +0200

    Translating Letters L and R to Left/Right Command.

--- src/MarsRover/Control/CommandTranslator.php
+++ src/MarsRover/Control/CommandTranslator.php
@@ -3,19 +3,25 @@ namespace MarsRover\Control;
 
 use MarsRover\Control\Commands\BackwardCommand;
 use MarsRover\Control\Commands\ForwardCommand;
+use MarsRover\Control\Commands\LeftCommand;
+use MarsRover\Control\Commands\RightCommand;
 
 class CommandTranslator
 {
+    private $letterToCommandMap;
+
     public function __construct()
     {
+        $this->letterToCommandMap = [
+            "f" => new ForwardCommand(),
+            "b" => new BackwardCommand(),
+            "l" => new LeftCommand(),
+            "r" => new RightCommand()
+        ];
     }
 
     public function translate($letter)
     {
-        if ($letter === 'f') {
-            return new ForwardCommand();
-        }
-
-        return new BackwardCommand();
+        return $this->letterToCommandMap[$letter];
     }
 }
--- test/MarsRover/Control/CommandTranslatorTest.php
+++ test/MarsRover/Control/CommandTranslatorTest.php
@@ -3,6 +3,8 @@ namespace MarsRover\Control;
 
 use MarsRover\Control\Commands\BackwardCommand;
 use MarsRover\Control\Commands\ForwardCommand;
+use MarsRover\Control\Commands\LeftCommand;
+use MarsRover\Control\Commands\RightCommand;
 
 class CommandTranslatorTest extends \PHPUnit_Framework_TestCase
 {
@@ -21,7 +23,9 @@ class CommandTranslatorTest extends \PHPUnit_Framework_TestCase
     {
         return [
             'Forward' => ['f', ForwardCommand::class],
-            'Backward' => ['b', BackwardCommand::class]
+            'Backward' => ['b', BackwardCommand::class],
+            'Left' => ['l', LeftCommand::class],
+            'Right' => ['r', RightCommand::class]
         ];
     }
 }
